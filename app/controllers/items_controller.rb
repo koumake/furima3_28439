84770679cb0f32449eff3b2cb6dd8eefda3675d2
koumake <koumake422@gmail.com>
2020-08-15 20:58:03 +0900

    コントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,42 @@
 class ItemsController < ApplicationController
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    Item.update(exhibition_params)
+  end
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
   def index
+    @item = Item.order(created_at: :DESC).includes(:user)
+    
+  end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+        redirect_to root_path
+    else
+        render :new
+    end
+  end
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+    
+       
+  
+  
+  
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :about, :price, :genre_id, :condition_id, :delivery_fee_id, :day_id, :prefecture_id).merge(user_id: current_user.id)
   end
 end
