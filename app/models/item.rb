3c236cfee3a358eb42923abf407694c47566b3e4
koumake <koumake422@gmail.com>
2020-08-15 20:45:35 +0900

    derivery_feeモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :genre, :condition
+    belongs_to_active_hash :genre, :condition, :delivery_fee
 
-    validates :genre, :condition_id, presence: true
+    validates :genre, :condition, :delivery_fee,  presence: true
     validates :genre_id, numericality: { other_than: 1 }
     validates :condition_id, numericality: { other_than: 1 }
+    validates :delivery_fee_id, numericality: { other_than: 1 }
 end
