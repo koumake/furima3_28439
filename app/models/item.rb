8d94a6698cf36752391a8901cef6645eafe32a16
koumake <koumake422@gmail.com>
2020-08-15 20:48:51 +0900

    prefectureモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :genre, :condition, :delivery_fee
+    belongs_to_active_hash :genre, :condition, :delivery_fee, :prefecture
 
-    validates :genre, :condition, :delivery_fee,  presence: true
+    validates :genre, :condition, :delivery_fee, :prefecture  presence: true
     validates :genre_id, numericality: { other_than: 1 }
     validates :condition_id, numericality: { other_than: 1 }
     validates :delivery_fee_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
 end
