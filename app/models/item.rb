9a009bb5ebdc9978801cabf09187bc83e8e8eafe
koumake <koumake422@gmail.com>
2020-08-15 20:38:23 +0900

    conditionモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :genre
+    belongs_to_active_hash :genre, :condition
 
-    validates :genre, presence: true
+    validates :genre, :condition_id, presence: true
     validates :genre_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
 end
