7d487f695d66b4d4d20dc6e0974f134ca4b60908
koumake <koumake422@gmail.com>
2020-08-15 21:18:40 +0900

    activestorage完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+    has_many :purchases
+    belongs_to :user
+    has_one_attached :image
+    
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :genre, :condition, :delivery_fee, :prefecture, :day
 
