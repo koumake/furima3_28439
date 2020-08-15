577e16c8b14dd65f1675f05dc146d5885db90b3a
koumake <koumake422@gmail.com>
2020-08-15 20:52:33 +0900

    dayモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :genre, :condition, :delivery_fee, :prefecture
+    belongs_to_active_hash :genre, :condition, :delivery_fee, :prefecture, :day
 
-    validates :genre, :condition, :delivery_fee, :prefecture  presence: true
+    validates :genre, :condition, :delivery_fee, :prefecture, :day  presence: true
     validates :genre_id, numericality: { other_than: 1 }
     validates :condition_id, numericality: { other_than: 1 }
     validates :delivery_fee_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
+    validates :day_id, numericality: { other_than: 1 }
 end
