b93813f974e55daf42fddd4fdf6852ed3a68dcfd
koumake <koumake422@gmail.com>
2020-08-15 20:33:18 +0900

    genreモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :genre
+
+    validates :genre, presence: true
+    validates :genre_id, numericality: { other_than: 1 }
 end
