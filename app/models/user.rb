e35dd777cc484c5453e37930fd60ee3edc1c7456
koumake <koumake422@gmail.com>
2020-08-15 14:41:57 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchases       
+        
+  with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: '6文字以上の半角英数字'
+    validates :email, uniqueness: true
+    validates :email, inclusion: { in: @ }
+    validates :firstname, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :lastname, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :firstname_alias, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :lastname_alias, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :password, confirmation: true
+  end      
+  validates :nickname, presence: true
 end
